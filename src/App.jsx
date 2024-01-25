0205dbee9357936b989a46449968ac8948df767d
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-25 18:56:13 +0100

    counter test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Starter from "./tutorial/01-useState/starter/01-error-example";
 
 function App() {
-  return <>hello</>;
+  return (
+    <div className='container'>
+      <Starter />
+    </div>
+  );
 }
 
 export default App;
--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [count, setCount] = useState(0);
+  function handleIncrease() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={handleIncrease}>increase</button>
+    </div>
+  );
 };
 
 export default ErrorExample;
