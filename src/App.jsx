bba35b81bd7495a337bd9688e8de3b307918de11
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-26 01:16:07 +0100

    list of name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Starter from "./tutorial/01-useState/starter/01-error-example";
+import Starter from "./tutorial/01-useState/starter/03-useState-array";
 
 function App() {
   return (
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const handleDelete = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+  const handleClearList = () => {
+    setPeople([]);
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <>
+            <h1 key={id}>{name}</h1>
+            <button onClick={() => handleDelete(id)}>remove</button>
+          </>
+        );
+      })}
+      <button onClick={handleClearList} className='btn'>
+        clear list
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
