cd4914e67795764c714980fb8a471f6be2bef48c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-26 13:02:46 +0100

    useState

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { people } from "../../../data";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("Ayodele");
+  const [hobby, setHobby] = useState("Singing");
+  const [age, setAge] = useState(35);
+  function handleChangeUser() {
+    setAge(40);
+    setName("Femi");
+    setHobby("playing Chess");
+  }
+  return (
+    <div>
+      {/* {people.map((person) => {
+        const { name, id } = person;
+        return <h1 key={id}>{name}</h1>;
+      })} */}
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{hobby}</h1>
+      <button type='button' onClick={handleChangeUser} className='btn'>
+        Change User
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
