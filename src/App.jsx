62975cbe989a6ecf5bb7f6d692cf1ebb30ac0632
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-12 15:29:44 +0100

    reinforcement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import Mpa from "./Mpa";
-import Starter from "./tutorial/02-useEffect/starter/01-code-example";
+// import Mpa from "./Mpa";
+import Starter from "./tutorial/01-useState/starter/04-useState-object";
 
 function App() {
   return (
     <div className='container'>
       <Starter />
-      <Mpa />
+      {/* <Mpa /> */}
     </div>
   );
 }
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { people } from "../../../data";
 
 const UseStateObject = () => {
+  const [list, setList] = useState(people);
   const [name, setName] = useState("Ayodele");
   const [hobby, setHobby] = useState("Singing");
   const [age, setAge] = useState(35);
@@ -10,12 +11,21 @@ const UseStateObject = () => {
     setName("Femi");
     setHobby("playing Chess");
   }
+  const deleteFunction = (id) => {
+    const neList = list.filter((person) => person.id !== id);
+    setList(neList);
+  };
   return (
     <div>
       <section className='people'>
-        {people.map((person) => {
+        {list.map((person) => {
           const { name, id } = person;
-          return <h1 key={id}>{name}</h1>;
+
+          return (
+            <h1 key={id}>
+              {name} <button onClick={() => deleteFunction(id)}>hi</button>
+            </h1>
+          );
         })}
       </section>
       <section className='person'>
