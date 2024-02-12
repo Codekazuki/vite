fc18d2e59aa19c369101cfa883c741b47bbb533a
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-12 14:21:41 +0100

    checking out

--- src/Mpa.jsx
+++ src/Mpa.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { data } from "./data";
+import { people } from "./data";
 const Mpa = () => {
   const [name, setName] = useState(data);
+  const [pipu, setPipu] = useState(people);
   const handleDelete = (id) => {
     console.log(id);
     const newPeople = name.filter((person) => person.id !== id);
@@ -24,6 +26,20 @@ const Mpa = () => {
         );
       })}
       <button onClick={handleDeleteAll}>clear all</button>
+
+      <div>
+        {pipu.map((people) => {
+          const { id, age, hobby, name } = people;
+          return (
+            <div key={id}>
+              <h1>{name}</h1>
+              <h2>{age}</h2>
+              <h3>{hobby}</h3>
+              <button>Chnage color</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
