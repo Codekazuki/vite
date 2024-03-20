9d6c2645f721e5ee49f2bf8c5a188f74411657ec
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-03-20 11:29:16 +0100

    innoliz class

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 // import Mpa from "./Mpa";
-import Starter from "./tutorial/01-useState/starter/04-useState-object";
+import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
 
 function App() {
   return (
