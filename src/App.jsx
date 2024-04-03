653fa4c48c3f42e220a457124495fb16c1e36ce2
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-04-03 04:41:29 +0100

    trying to commit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Starter from "./tutorial/01-useState/starter/04-useState-object";
+import Starter from "./tutorial/02-useEffect/starter/01-code-example";
 
 function App() {
   return (
--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
   return (
     <div>
-      <h1>value : {value}</h1>
+      <h1>count : {value}</h1>
       <button className='btn' onClick={() => setValue(value + 1)}>
         click me
       </button>
