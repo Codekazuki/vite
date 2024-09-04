d2fd118f2c541269c066ab382e967dcc4adda06f
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-04 14:56:48 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
-// import Mpa from "./Mpa";
+
 import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
 
 function App() {
   return (
     <div className='container'>
       <Starter />
-      {/* <Mpa /> */}
     </div>
   );
 }
