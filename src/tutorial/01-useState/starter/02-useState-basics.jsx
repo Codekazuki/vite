b1955685ec414260ec0c58093cddf1fa7907e350
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-05 12:20:52 +0100

    sprint

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { data } from "../../../data";
 
 const UseStateBasics = () => {
   const [people, setPeople] = useState(data);
 
-  const handleClearAll = () => {
+  const handleClrearList = () => {
     setPeople([]);
   };
-  const handleDelete = () => {
-    console.log("hi");
-  };
+  function deleteName(id) {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  }
 
   return (
     <div>
@@ -18,12 +19,12 @@ const UseStateBasics = () => {
         return (
           <div key={id}>
             <h1>{name}</h1>
-            <button onClick={handleDelete}>delete name</button>
+            <button onClick={() => deleteName(id)}>remove</button>
           </div>
         );
       })}
-      <button className='btn' onClick={handleClearAll}>
-        DELETE ALL
+      <button onClick={handleClrearList} className='btn'>
+        Clearlist
       </button>
     </div>
   );
