f0814af6a2f23dd89213230adb200759962cacca
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-08 20:02:11 +0100

    sprint

--- src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
   const sayHello = () => {
     console.log("hello there");
   };
 
   sayHello();
+  const doubleCount = () => {
+    setValue(value * 2);
+  };
   useEffect(() => {
     console.log("effect here");
-  }, []);
+  }, [doubleCount]);
 
   return (
     <div>
       <h1>count : {value}</h1>
       <button className='btn' onClick={() => setValue(value + 1)}>
         click me to Increase.
+      </button>
+      <button className='btn' onClick={() => doubleCount}>
+        double Count
       </button>
     </div>
   );
