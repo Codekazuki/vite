ee19cd83e383a6ff41e8e6e2d5907c922101f44c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-09 15:57:39 +0100

    sprint

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  if (isLoading) {
+    return <h1>data is loading, please calm down</h1>;
+  }
+  return <h2>Multiple Returns Basics @ conditional rendering </h2>;
 };
 export default MultipleReturnsBasics;
