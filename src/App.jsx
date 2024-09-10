908a2b44c7008205c4616c774f4aa9ddfc49afdf
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-10 06:33:57 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
 import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
-
-  if (isLoading) {
-    return <h1>data is loading</h1>;
-  }
   return (
     <div className='container'>
       <Starter />
