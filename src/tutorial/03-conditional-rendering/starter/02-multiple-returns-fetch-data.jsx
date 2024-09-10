9d7121186173cf92c14e64e75a2636b39d46bb4d
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-10 08:43:58 +0100

    sprint

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,29 +3,37 @@ const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const resp = await fetch(url);
         const user = await resp.json();
+        setUser(user);
         console.log(user);
       } catch (error) {
         console.log("there is error");
+        setIsError(true);
       }
+      setIsLoading(false);
     };
     fetchUser();
   }, []);
-
-  return user.map((user) => {
-    const { id, bio, avatar_url } = user;
-    return (
-      <div key={id}>
-        <img src={avatar_url} alt='' />
-        <h1>{bio}</h1>
-        <p></p>
-      </div>
-    );
-  });
+  if (isLoading) {
+    return <h1>data is loading</h1>;
+  }
+  if (isError) {
+    return <h2>Ooops! en error just occured</h2>;
+  }
+  return (
+    <div>
+      <img src={user.avatar_url} alt='' />
+      <h1>{user.name}</h1>
+      <h4>{user.bio}</h4>
+      <h4>{user.location}</h4>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
