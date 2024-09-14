be79289bd7b2bb314570d23b30cbf1c84d569ea2
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-14 09:25:53 +0100

    sprint

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,8 +1,45 @@
 const ControlledInputs = () => {
   return (
     <form className='form'>
-      <h1>Register</h1>
-      <input type='text' className='form-input' placeholder='input your name' />
+      <h1>Controlled Input</h1>
+
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          name
+        </label>
+        <input
+          id='name'
+          type='text'
+          className='form-input'
+          placeholder='input your name'
+        />
+      </div>
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          email
+        </label>
+        <input
+          id='email'
+          type='email'
+          className='form-input'
+          placeholder='input your name'
+        />
+      </div>
+      {/* <div className='form-row'>
+        <label htmlFor='password' className='form-label'>
+          Password
+        </label>
+        <input
+          id='password'
+          type='password'
+          className='form-input'
+          placeholder='input your name'
+        />
+      </div> */}
+      <button type='submit' className='btn btn-block'>
+        {" "}
+        submit
+      </button>
     </form>
   );
 };
