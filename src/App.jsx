1637cf4bd2f3717e75c0567dc54ad3260a9df813
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-16 13:31:46 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "../src/index.css";
 
-import Starter from "./tutorial/06-forms/starter/02-user-challenge";
+import Starter from "./tutorial/06-forms/starter/04-other-inputs";
 
 function App() {
   return (
