83e1dbf56dc29727e63392878c631dffdad76e6b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-16 14:12:01 +0100

    sprint

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 const frameworks = ["react", "angular", "vue", "svelte"];
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState("react");
   const handleShipping = (e) => {
     setShipping(e.target.checked);
-    console.log(e.target.checked);
+  };
+
+  const handleFramework = (e) => {
+    setFramework(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(shipping, framework);
+    setShipping(false);
+    setFramework("react");
   };
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Other Inputs</h4>
         {/* name */}
         <div className='form-row' style={{ textAlign: "left" }}>
@@ -25,6 +35,22 @@ const OtherInputs = () => {
           <label htmlFor='framework' className='form-label'>
             Framework
           </label>
+
+          <select
+            name='framework'
+            id='framework'
+            value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((framework) => {
+              return (
+                <option key={framework} value={framework}>
+                  {" "}
+                  {framework}{" "}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <button type='submit' className='btn btn-block'>
           submit
